cb4c84d88483ee35e37e7059a70f1894c0a647c9
Xivo <lai.xavier3108@gmail.com>
2026-01-29 14:27:08 +0100

    Delete vrai directory

--- vrai/utils.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import shared_memory, resource_tracker
-
-def fix_tracker(shm):
-    """Empêche Python de supprimer la mémoire quand le processus s'arrête."""
-    try:
-        resource_tracker.unregister(shm._name, 'shared_memory')
-    except Exception:
-        pass
